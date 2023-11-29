d561df63f477627aec9591ad0d981e45ff57f80a
unknown <phivanduc325@gmail.com>
2023-11-29 21:13:46 +0700

    Add file connectDatabase.php

--- database/BTTH01_CSE485.sql
+++ database/BTTH01_CSE485.sql
@@ -169,7 +169,3 @@ VALUES (
 "Ánh nắng cuối ngày rồi cũng sẽ tắt, dòng sông con đò rồi cũng sẽ rẽ sang một hướng khác. Nhưng việc trồng người luôn cảm thụ với chuyến đò ngang, cứ tần tảo đưa rồi lặng lẽ quay về đưa sang. Con đò năm xưa của Thầy nặng trĩu yêu thương, hy sinh thầm lặng.",
 8, "2014/1/2"
 );
-
-
-
-
